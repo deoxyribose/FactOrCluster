0137788e1ee9a80ab5acbb170841862cb094c429
Rasmus Bonnevie <rabo@dtu.dk>
2018-08-27 20:42:14 +0200

    Fixed reference errors after renaming.

--- tfpmodels.py
+++ tfpmodels.py
@@ -24,7 +24,7 @@ def ifa(N = 1000, K = 2, C = 3, P = 4):
     mixture_component_means = ed.Normal(loc=0., scale=1., sample_shape=(C,K), name='mixture_component_means')
     mixture_component_std = ed.InverseGamma(concentration=1., rate=1., sample_shape=(C,K), name='mixture_component_std')
     mixture_weights = ed.Dirichlet(concentration=np.ones(K), sample_shape=(C,), name='mixture_weights')
-    z = ed.Independent(
+    sources = ed.Independent(
         tfd.MixtureSameFamily(
             mixture_distribution=tfd.Categorical(probs=mixture_weights),
             components_distribution=tfd.Normal(loc=mixture_component_means, scale=mixture_component_std, name='mixture_component')),
@@ -32,7 +32,7 @@ def ifa(N = 1000, K = 2, C = 3, P = 4):
     factor_loadings = ed.Normal(loc=0., scale=1., sample_shape=(C, P), name='factor_loadings')
     data_mean = tf.matmul(sources, factor_loadings, name='data_mean')
     data_variance = ed.InverseGamma(concentration=1., rate=1., sample_shape=(1,P), name='data_variance')
-    data = ed.Normal(loc=X, scale=data_variance, name='data')  
+    data = ed.Normal(loc=data_mean, scale=data_variance, name='data')  
     return data
 
 def cifa(N = 1000, K = 2, C = 3, P = 4):
@@ -46,9 +46,9 @@ def cifa(N = 1000, K = 2, C = 3, P = 4):
     factor_loadings = ed.Normal(loc=0., scale=1., sample_shape=(C, P), name='factor_loadings')
     data_mean = tf.matmul(sources, factor_loadings, name='data_mean')
     data_variance = ed.InverseGamma(concentration=1., rate=1., sample_shape=(1,P), name='data_variance')
-    data = ed.Normal(loc=X, scale=data_variance, name='data')  
+    data = ed.Normal(loc=data_mean, scale=data_variance, name='data')  
     return data
 
-
-model = cifa(2000, 2, 2, 2)
+cmodel = ifa(10000, 2, 2, 2)
+cmodel = cifa(10000, 2, 2, 2)
     
