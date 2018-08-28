c1faf95919cf2a41faed4c55ba1bc59d7fc114fc
Rasmus Bonnevie <rabo@dtu.dk>
2018-08-28 16:29:21 +0200

    Added testsuite.py and modified map.py
    Changed Mapper so that it does not instantiate observed variables.
    Also augmented it with a method that returns a MAP optimizer.
    Added testsuite.py which provides an initial testbed for testing models.

--- map.py
+++ map.py
@@ -12,18 +12,21 @@ class Mapper:
     _positive_distributions = [tfd.InverseGamma, tfd.Gamma]
     _simplex_distributions = [tfd.Dirichlet]
 
-    def __init__(self, model, *args, **kwargs):
+    def __init__(self, model, model_name, observed_variable_names, *args, **kwargs):
         self.model = model
+        self.model_name = model_name
+        self.observed_variable_names = observed_variable_names
         self.loss_fn = ed.make_log_joint_fn(model) 
         self._args = args
         self._kwargs = kwargs
         with tape() as self.tape:
-            self.model(*args, **kwargs)
-        self.variable_names = self.tape.keys()
-        self.variable_shapes = {key: val.shape for key, val in self.tape.items()}
-        self.variable_dist = {key: val.distribution for key, val in self.tape.items()}
-        self.unconstrained_variables = {key: tf.get_variable(key, shape=self.variable_shapes[key]) for key in self.tape.keys()}
-        self.variables = {key: self.wrap(val, self.variable_dist[key]) for key, val in self.unconstrained_variables.items()}
+            self.output = self.model(*args, **kwargs)
+        with tf.variable_scope(self.model_name, reuse=tf.AUTO_REUSE):
+            self.variable_names = [key for key in self.tape.keys() if key not in self.observed_variable_names]
+            self.variable_shapes = {key: self.tape[key].shape for key in self.variable_names}
+            self.variable_dist = {key: self.tape[key].distribution for key in self.variable_names}
+            self.unconstrained_variables = {key: tf.get_variable(key, shape=self.variable_shapes[key]) for key in self.variable_names}
+            self.variables = {key: self.wrap(val, self.variable_dist[key]) for key, val in self.unconstrained_variables.items()}
         
     def wrap(self, variable, distribution):
         if distribution.__class__ in self._positive_distributions:
@@ -32,5 +35,9 @@ class Mapper:
             return tf.nn.softmax(variable)
         return variable
 
-    def map_loss(self):
-        return self.loss_fn(*self._args, **self._kwargs, **self.variables)
+    def map_loss(self, **kwargs):
+        return self.loss_fn(*self._args, **self._kwargs, **self.variables, **kwargs)
+
+    def map_optimizer(self, **kwargs):
+        loss = -self.map_loss(**kwargs)
+        return loss, tf.contrib.opt.ScipyOptimizerInterface(loss, self.unconstrained_variables.values())
