b36910faf69dc1ebbedcf2c65fca5eb04a325bac
Rasmus Bonnevie <rabo@dtu.dk>
2018-08-29 12:45:33 +0200

    Fixed testsuite.py. Added loop  over deviation.

--- testsuite.py
+++ testsuite.py
@@ -5,28 +5,46 @@ ed = tfp.edward2
 import numpy as np
 
 from future_features import tape
-from map import Mapper
+from mapper import Mapper
 
 from tfpmodels import centeredIndependentFactorAnalysis, mixtureOfGaussians
 
-N = 10000
-Ntest = 1000
+import pandas as pd
+import xarray as xr
 
-n_features = 10
+N = 100
+Ntest = 0
 
-cifa_2_3 = Mapper(centeredIndependentFactorAnalysis, 'cifa2', observed_variable_names=['data'], n_observations=N, n_components_in_mixture = 2, n_sources=3, n_features=n_features)
-mog_3 = Mapper(mixtureOfGaussians, 'mog', observed_variable_names=['data'], n_observations=N, n_components=3, n_features=n_features)
+n_features = 4
+n_restarts = 10
+n_datasets = 10
 
-models = [cifa_2_3, mog_3]
+deviations = np.logspace(-3,1,5, dtype='float32')
+
+cifa_2_2 = Mapper(centeredIndependentFactorAnalysis, 'cifa', observed_variable_names=['data'], n_observations=N, n_components_in_mixture = 2, n_sources=2, n_features=n_features)
+mog_4 = Mapper(mixtureOfGaussians, 'mog', observed_variable_names=['data'], n_observations=N, n_components=4, n_features=n_features)
+
+models = [cifa_2_2, mog_4]
+model_names = [model.model_name for model in models]
+
+results = xr.DataArray(np.zeros((len(models), len(deviations), n_restarts, n_datasets)),dims=['model', 'deviation', 'restart', 'dataset'], coords={'model': model_names, 'deviation': deviations, 'restart': range(n_restarts), 'dataset': range(n_datasets)})
+
+placeholder_deviation = tf.placeholder(dtype='float32')
+with tape() as reference_tf:
+    data_tf = mixtureOfGaussians(n_observations=N + Ntest, n_components=3, n_features=n_features, mixture_component_means_mean=placeholder_deviation)
+        
 
 with tf.Session() as sess:
-    with tape() as reference_tf:
-        data_tf = mixtureOfGaussians(n_observations=N, n_components=3, n_features=n_features)
-    data, reference = sess.run([data_tf, reference_tf])
+    for deviation in deviations:
+        for dataset in range(n_datasets):
+            data, reference = sess.run([data_tf, reference_tf], feed_dict={placeholder_deviation: deviation})
 
-    opt = {}
-    for model in models: 
-        _, opt[model.model_name] = model.map_optimizer(data=data)
-    sess.run(tf.global_variables_initializer())
-    for model in models: 
-        opt[model.model_name].minimize()
+            loss = {}
+            opt = {}
+            for model in models: 
+                loss[model.model_name], opt[model.model_name] = model.map_optimizer(data=data[:N])
+            for restart in range(n_restarts):        
+                sess.run(tf.global_variables_initializer())
+                for model in models: 
+                    opt[model.model_name].minimize()
+                    results.loc[{'model': model.model_name, 'deviation': deviation, 'restart': restart, 'dataset': dataset}] = sess.run(loss[model.model_name])
