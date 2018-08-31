36dfeedfa969ee1dbb1235250269b11d91a05f04
Rasmus Bonnevie <rabo@dtu.dk>
2018-08-31 13:56:49 +0200

    Modified constrained parameters to use bijectors.
    The Mapper class now registers a bijection in self.transforms that can
    be used to go between the constrained and unconstrained variable.
    Assigner now maps assignments on the constrained variables to the
    unconstrained variables using the transformers.
    A bijector bug made it necessary to implement a temporary fix in
    future_features.py.

--- mapper.py
+++ mapper.py
@@ -1,14 +1,13 @@
 import tensorflow as tf
 import tensorflow_probability as tfp
 tfd = tfp.distributions
+tfb = tfp.bijectors
 ed = tfp.edward2
 import numpy as np
 
-from future_features import tape
-
+from future_features import tape, SoftmaxCentered
 
 class Mapper:
-
     _positive_distributions = [tfd.InverseGamma, tfd.Gamma]
     _simplex_distributions = [tfd.Dirichlet]
     _tril_distributions = [tfd.Wishart]
@@ -24,22 +23,23 @@ class Mapper:
             self.output = self.model(*args, **kwargs)
         with tf.variable_scope(self.model_name, reuse=tf.AUTO_REUSE):
             self.variable_names = [key for key in self.tape.keys() if key not in self.observed_variable_names]
-            self.variable_shapes = {key: self.tape[key].shape for key in self.variable_names}
             self.variable_dist = {key: self.tape[key].distribution for key in self.variable_names}
-            self.unconstrained_variables = {key: tf.get_variable(key, shape=self.variable_shapes[key]) for key in self.variable_names}
-            self.variables = {key: self.wrap(val, self.variable_dist[key]) for key, val in self.unconstrained_variables.items()}
+            self.variable_shapes = {key: self.tape[key].shape for key in self.variable_names}
+            self.transforms = {key: self.get_bijector(self.tape[key]) for key in self.variable_names}
+            self.unconstrained_variable_shapes = {key: self.transforms[key].inverse_event_shape(val) for key, val in self.variable_shapes.items()}
+            self.unconstrained_variables = {key: tf.get_variable(key, shape=self.unconstrained_variable_shapes[key]) for key in self.variable_names}
+            self.variables = {key: self.transforms[key].forward(val) for key, val in self.unconstrained_variables.items()}
         
-    def wrap(self, variable, distribution):
+    def get_bijector(self, random_variable):
+        distribution = random_variable.distribution
         if distribution.__class__ in self._positive_distributions:
-            return tfp.trainable_distributions.softplus_and_shift(variable)
-        if distribution.__class__ in self._simplex_distributions:
-            return tf.nn.softmax(variable)
-        if distribution.__class__ in self._tril_distributions:
-            batch = variable.shape[0]
-            dim = variable.shape[1]
-            trildim = dim*(dim+1)//2
-            return tfp.trainable_distributions.tril_with_diag_softplus_and_shift(tf.reshape(variable,(batch,-1))[:,:trildim])
-        return variable
+            return tfb.Softplus() #tfp.trainable_distributions.softplus_and_shift(variable)
+        elif distribution.__class__ in self._simplex_distributions:
+            return SoftmaxCentered()
+        elif distribution.__class__ in self._tril_distributions:
+            return tfb.ScaleTriL()
+        else:
+            return tfb.Identity()
 
     def map_neg_log_joint_fn(self, **kwargs):
         return -self.log_joint_fn(*self._args, **self._kwargs, **self.variables, **kwargs)
@@ -51,5 +51,5 @@ class Mapper:
     def assigner(self, **kwargs):
         assign_ops = []
         for key, val in kwargs.items():
-            assign_ops.append(tf.assign(self.unconstrained_variables[key], val))
+            assign_ops.append(tf.assign(self.unconstrained_variables[key], self.transforms[key].inverse(val)))
         return tf.group(assign_ops)
