d26a801af671832573e2b11474cf3e8f3c8a6ad2
Rasmus Bonnevie <rabo@dtu.dk>
2018-09-28 14:41:46 +0200

    Model made conjugate and EM-compliant, with EM and MAP-EM functionality.
    mapper.py modified with the IFA_MAPEM class for fitting (c)IFA with
    MAP-EM.
    CIFA modified to be conjugate with inverse gamma priors and
    isotropic noise.
    Notebook for EM added.

--- mapper.py
+++ mapper.py
@@ -4,11 +4,13 @@ tfd = tfp.distributions
 tfb = tfp.bijectors
 ed = tfp.edward2
 import numpy as np
-
+from itertools import product
 from future_features import tape, SoftmaxCentered
 
+from tfpmodels import centeredMarginalizedIndependentFactorAnalysisTest
 from tfopt import PylbfgsInterface
 
+import pdb
 
 class Mapper:
     _positive_distributions = [tfd.InverseGamma, tfd.Gamma]
@@ -36,8 +38,10 @@ class Mapper:
 
 
     def get_bijector(self, random_variable):
-        positivity_shift = tf.convert_to_tensor(1e-3,dtype=tf.float64)
-        diag_shift = tf.convert_to_tensor(1.,dtype=tf.float64)
+        positivity_shift = tf.convert_to_tensor(0.,dtype=tf.float64)
+        diag_shift = tf.convert_to_tensor(0.,dtype=tf.float64)
+        #positivity_shift = tf.convert_to_tensor(1e-3,dtype=tf.float64)
+        #diag_shift = tf.convert_to_tensor(1.,dtype=tf.float64)
         
         distribution = random_variable.distribution
         if distribution.__class__ in self._positive_distributions:
@@ -98,10 +102,94 @@ class Mapper:
             assign_ops.append(tf.assign(self.unconstrained_variables[key], self.transforms[key].inverse(val)))
         return tf.group(assign_ops)
 
-#class IFA_EM(Mapper):
-#    def __init__(self, model, model_name, observed_variable_names, *args, **kwargs):
-#        super(IFA_EM).__init__(model, model_name, observed_variable_names, *args, **kwargs)
+class IFA_MAPEM(Mapper):
+    def __init__(self, model, model_name, observed_variable_names, *args, **kwargs):
+        super().__init__(model, model_name, observed_variable_names, *args, **kwargs)
+        self.current = {key: tf.Variable(initial_value=tf.zeros(val, dtype='float64')) for key, val in self.variable_shapes.items()}
 
+    def MAPEMsteps(self, data):
+        mixture_weights = self.current['mixture_weights']
+        mixture_component_var = self.current['mixture_component_var']
+        factor_loadings = self.current['factor_loadings']
+        data_var = self.current['data_var']
         
-        #self.posterior_eval = 
-        #self.po
+        jitter = 0. #jitter is not automatically matched!
+        data = tf.convert_to_tensor(data, dtype='float64')
+        n_observations = data.shape[0].value
+        n_sources, n_components_in_mixture = mixture_component_var.shape.as_list()
+        n_features = factor_loadings.shape[1].value
+
+        combinations = tf.one_hot(np.array(list(
+            product(range(n_components_in_mixture), repeat=n_sources))).astype('float64'), 
+            depth=n_components_in_mixture, dtype='float64') # combinations x sources x component indicator
+        n_combinations = combinations.shape[0]
+        all_mixture_weights = tf.reduce_prod(tf.reduce_sum(combinations * mixture_weights[None, :, :], axis=-1), axis=1)
+        all_mixture_vars = tf.reduce_sum(combinations * mixture_component_var[None, :, :], axis=-1)
+
+        all_mixture_cov_factor = tf.einsum('sf,cs->csf', factor_loadings, tf.sqrt(all_mixture_vars))
+        all_mixture_cov = tf.einsum('csf,csg->cfg', all_mixture_cov_factor, all_mixture_cov_factor) + (jitter + data_var)*tf.eye(n_features, dtype='float64')[None,:,:]
+        all_mixture_cov_chol = tf.cholesky(all_mixture_cov)
+
+        marginal_eval = ed.MultivariateNormalTriL(loc=tf.zeros((n_combinations, n_features), dtype='float64'),
+                                                scale_tril=all_mixture_cov_chol, sample_shape=(n_observations,)).distribution.log_prob(data[:,None,:])
+        log_posterior_assignment = tf.log(all_mixture_weights)[None,:] + marginal_eval
+        log_posterior_assignment -= tf.reduce_logsumexp(log_posterior_assignment, axis=-1, keepdims=True)
+        posterior_assignment = tf.exp(log_posterior_assignment)
+
+        posterior_component_inv_cov = tf.matmul(factor_loadings, factor_loadings * (1./data_var), transpose_b=True) + tf.map_fn(tf.diag, 1./all_mixture_vars)
+        posterior_component_inv_cov_chol = tf.cholesky(posterior_component_inv_cov)
+        posterior_component_cov = tf.stack([tf.transpose(tf.cholesky_solve(L, tf.eye(n_sources, dtype='float64'))) for L in tf.unstack(posterior_component_inv_cov_chol)], axis=0)
+        
+        posterior_component_mean = tf.stack([tf.transpose(tf.cholesky_solve(L, tf.matmul(factor_loadings, data / data_var, transpose_b=True))) for L in tf.unstack(posterior_component_inv_cov_chol)], axis=1)
+        posterior_component_second_moment = posterior_component_cov[None,:,:,:] + tf.einsum('nqk,nql->nqkl', posterior_component_mean, posterior_component_mean) 
+        posterior_component_second_moment_diag = tf.einsum('nqij,nqij->nqi', posterior_component_second_moment,
+            tf.tile(tf.eye(n_sources, dtype='float64')[None, None, :, :], [n_observations, n_combinations, 1,1]))
+
+        posterior_marginal_source_mean = tf.einsum('nq,nqi->ni', posterior_assignment, posterior_component_mean)
+        posterior_marginal_source_second_moment = tf.einsum('nq,nqij->nij', posterior_assignment, posterior_component_second_moment)
+        posterior_marginal_source_second_moment_sum = tf.reduce_sum(posterior_marginal_source_second_moment, axis=0)
+        
+        updates = {}
+        #does not handle arbitrary mean
+        concentration = self.tape['mixture_weights'].distribution.parameters['concentration']
+        updates['mixture_weights'] = (concentration[None,:] - 1. + tf.einsum('qkc,nq->kc', combinations, posterior_assignment))/(n_observations + tf.reduce_sum(concentration - 1.))
+        
+        concentration = self.tape['mixture_component_var'].distribution.parameters['concentration']
+        rate = self.tape['mixture_component_var'].distribution.parameters['rate']
+        updates['mixture_component_var'] = (2.*rate + tf.reduce_sum(tf.einsum('qkc,nq,nqk->qkc', combinations, posterior_assignment, posterior_component_second_moment_diag), axis=0))/(2.*(1.+concentration) + tf.einsum('qkc,nq->kc', combinations, posterior_assignment))
+        #updates['mixture_component_var'] = (2.*rate + tf.einsum('qkc,nq,nk->kc', combinations, posterior_assignment, posterior_marginal_source_cov_diag + tf.square(posterior_marginal_source_mean)))/(2.*(1.+concentration) + tf.einsum('qkc,nq->kc', combinations, posterior_assignment))
+        
+        scale = self.tape['factor_loadings'].distribution.parameters['scale']
+        updates['factor_loadings'] = (tf.linalg.cholesky_solve(tf.linalg.cholesky(posterior_marginal_source_second_moment_sum + (data_var/tf.square(scale))*tf.eye(n_sources, dtype='float64')), tf.einsum('ns,nd->sd',posterior_marginal_source_mean,data)))
+        #updates['factor_loadings'] = tf.transpose(tf.linalg.solve(posterior_marginal_source_second_moment_sum, tf.transpose(tf.matmul(data, posterior_marginal_source_mean, transpose_a=True))))
+        
+        concentration = self.tape['data_var'].distribution.parameters['concentration']
+        rate = self.tape['data_var'].distribution.parameters['rate']
+        updates['data_var'] = (tf.reduce_sum(tf.square(data)) + 
+                               tf.reduce_sum(tf.matmul(factor_loadings, factor_loadings, transpose_b=True) * posterior_marginal_source_second_moment_sum) - 
+                               2.*tf.reduce_sum(data * tf.matmul(posterior_marginal_source_mean, factor_loadings)) + 
+                               2.*rate)/(n_observations*n_features+2.*(1.+concentration))    
+        return updates
+
+    def MAPEM_optimizer(self, **kwargs):
+        set_current = tf.group([tf.assign(self.current[key], self.variables[key]) for key in self.current.keys()])
+        steps = self.assigner(**self.MAPEMsteps(data=kwargs['data']))
+        map_neg_log_joint = self.map_neg_log_joint_fn(**kwargs)
+        
+        return map_neg_log_joint, set_current, steps
+    
+    def MAPEM_enhanced_map_neg_log_joint_fn(self, **kwargs):
+        set_current = tf.group([tf.assign(self.current[key], self.variables[key]) for key in self.current.keys()])
+        with tf.control_dependencies([set_current]):
+            emsteps = self.MAPEMsteps(data=kwargs['data'])
+        regularizer = tf.reduce_sum([tf.linalg.norm(emsteps[key] - self.variables[key]) for key in emsteps.keys])
+        return -self.log_joint_fn(*self._args, **self._kwargs, **emsteps, **kwargs) + regularizer
+
+    def bfgs_optimizer(self, enhanced=False, **kwargs):
+        if enhanced:
+            map_neg_log_joint = self.MAPEM_enhanced_map_neg_log_joint_fn(**kwargs)
+        else:
+            map_neg_log_joint = self.map_neg_log_joint_fn(**kwargs)
+        return map_neg_log_joint, tf.contrib.opt.ScipyOptimizerInterface(map_neg_log_joint, self.unconstrained_variables.values(), method='BFGS')
+   
+                
--- tfpmodels.py
+++ tfpmodels.py
@@ -7,8 +7,7 @@ import numpy as np
 from itertools import product
 from future_features import tape
 
-jitter = 1e-04
-print(jitter)
+jitter = 0.
 
 def independentFactorAnalysis(n_observations = 1000, n_components_in_mixture = 2, n_sources = 2, n_features = 2, mixture_component_means_mean = 0., mixture_component_means_var = 1., mixture_component_var_concentration = 1., mixture_component_var_rate=1.,mixture_weights_concentration=None,data_var_concentration=1.,data_var_rate=1.):
     if mixture_weights_concentration is None:
@@ -43,22 +42,16 @@ def centeredIndependentFactorAnalysis(n_observations = 1000, n_components_in_mix
     data = ed.Normal(loc=data_mean, scale=tf.sqrt(data_var), name='data')  
     return data
 
-
-def condition_number(matrix):
-    singular_values = tf.svd(matrix,compute_uv=False)
-    largest_singular_value = tf.reduce_max(singular_values,axis=1)
-    smallest_singular_value = tf.reduce_max(singular_values,axis=1)
-    return largest_singular_value,smallest_singular_value,largest_singular_value/smallest_singular_value
-
-def centeredMarginalizedIndependentFactorAnalysis(n_observations = 1000, n_sources = 2, n_components_in_mixture=2, n_features = 2, mixture_component_var_concentration = 1., mixture_component_var_rate=1.,mixture_weights_concentration=None,data_var_concentration=1.,data_var_rate=1.):
-
+def centeredMarginalizedIndependentFactorAnalysis(n_observations = 1000, n_sources = 2, n_components_in_mixture=2, 
+    n_features = 2, mixture_component_var_concentration = 1., mixture_component_var_rate=1., 
+    mixture_weights_concentration=None, data_var_concentration=1., data_var_rate=1.):
     if mixture_weights_concentration is None:
         mixture_weights_concentration = np.ones(n_components_in_mixture, dtype='float64')
     combinations = tf.one_hot(np.array(list(
         product(range(n_components_in_mixture), repeat=n_sources))).astype('float64'), 
         depth=n_components_in_mixture, dtype='float64') # combinations x sources x component indicator
     n_combinations = combinations.shape[0]
-    mixture_component_var = ed.Gamma(concentration=tf.convert_to_tensor(mixture_component_var_concentration, dtype='float64'),
+    mixture_component_var = ed.InverseGamma(concentration=tf.convert_to_tensor(mixture_component_var_concentration, dtype='float64'),
         rate=tf.convert_to_tensor(mixture_component_var_rate, dtype='float64'),
         sample_shape=(n_sources,n_components_in_mixture), name='mixture_component_var')
     mixture_weights = ed.Dirichlet(concentration=tf.convert_to_tensor(mixture_weights_concentration, dtype='float64'),
@@ -66,39 +59,30 @@ def centeredMarginalizedIndependentFactorAnalysis(n_observations = 1000, n_sourc
     factor_loadings = ed.Normal(loc=tf.convert_to_tensor(0., dtype='float64'), 
         scale=tf.convert_to_tensor(1., dtype='float64'), 
         sample_shape=(n_sources, n_features), name='factor_loadings')
-    #factor_loadings /= tf.linalg.norm(factor_loadings, axis=1, keepdims=True)
-    data_var = ed.Gamma(concentration=tf.convert_to_tensor(data_var_concentration, dtype='float64'),
+    data_var = ed.InverseGamma(concentration=tf.convert_to_tensor(data_var_concentration, dtype='float64'),
         rate=tf.convert_to_tensor(data_var_rate, dtype='float64'),
-        sample_shape=(n_features,), name='data_var')
+        sample_shape=(), name='data_var')
     
     all_mixture_weights = tf.reduce_prod(tf.reduce_sum(combinations * mixture_weights[None, :, :], axis=-1), axis=1)
     all_mixture_vars = tf.reduce_sum(combinations * mixture_component_var[None, :, :], axis=-1)
 
-    cov_factor = tf.einsum('sf,cs->csf', factor_loadings, all_mixture_vars)
-    covmat = tf.einsum('csf,csg->cfg', cov_factor, cov_factor) + tf.diag(jitter + data_var)[None,:,:]
+    cov_factor = tf.einsum('sf,cs->csf', factor_loadings, tf.sqrt(all_mixture_vars))
+    covmat = tf.einsum('csf,csg->cfg', cov_factor, cov_factor) + (jitter + data_var)*tf.eye(n_features, dtype='float64')[None,:,:]
+    #diag noise: covmat = tf.einsum('csf,csg->cfg', cov_factor, cov_factor) + tf.diag(jitter + data_var)[None,:,:]
     
-    # conditioning of covariance matrices
-    #largest_magnitude_covariance = tf.reduce_max(tf.abs(covmat),axis=[1,2],keepdims=True)
-    #covmat /= largest_magnitude_covariance
-    #covmat = tf.cast(covmat,tf.float64)
-    #covmat /= tf.Print(largest_magnitude_covariance,[largest_magnitude_covariance],summarize=100,message='\n Normalizing covmats by')
-
     data = ed.MixtureSameFamily(
         mixture_distribution=tfd.Categorical(probs=all_mixture_weights),
         components_distribution=tfd.MultivariateNormalTriL(
             loc=tf.zeros((n_combinations, n_features),dtype=tf.float64),
             scale_tril=tf.cholesky(covmat),
-            #scale_tril=tf.cast(tf.sqrt(largest_magnitude_covariance),tf.float64)*tf.linalg.cholesky(tf.Print(covmat,[covmat],summarize=100)),
-            #scale_tril=tf.sqrt(largest_magnitude_covariance)*tf.linalg.cholesky(tf.Print(covmat,[covmat],summarize=100)),
-            #scale_tril=tf.sqrt(largest_magnitude_covariance)*tf.linalg.cholesky(tf.Print(covmat, [tf.check_numerics(covmat,message='covmat is nan or inf'),'Determinants:',tf.linalg.det(covmat),'max_singvals,min_singvals,cond_numbers::',condition_number(covmat)],summarize=100,message='\n')),
             name='data_component'),
             sample_shape=(n_observations,), name='data')
     return data
 
 
 def centeredMarginalizedIndependentFactorAnalysisTest(n_observations, mixture_weights, mixture_component_var, factor_loadings, data_var):
-    n_sources, n_features = factor_loadings.shape
-    n_components_in_mixture = mixture_weights.shape[1]
+    n_sources, n_features = tf.convert_to_tensor(factor_loadings).shape.as_list()
+    n_components_in_mixture = tf.convert_to_tensor(mixture_weights).shape[1].value
     combinations = tf.one_hot(np.array(list(
         product(range(n_components_in_mixture), repeat=n_sources))).astype('float64'), 
         depth=n_components_in_mixture, dtype='float64') # combinations x sources x component indicator
@@ -106,41 +90,17 @@ def centeredMarginalizedIndependentFactorAnalysisTest(n_observations, mixture_we
 
     all_mixture_weights = tf.reduce_prod(tf.reduce_sum(combinations * mixture_weights[None, :, :], axis=-1), axis=1)
     all_mixture_vars = tf.reduce_sum(combinations * mixture_component_var, axis=-1)
-    #factor_loadings /= tf.linalg.norm(factor_loadings, axis=1, keepdims=True)
-    cov_factor = tf.einsum('sf,cs->csf', factor_loadings, all_mixture_vars)
-    covmat = tf.einsum('csf,csg->cfg', cov_factor, cov_factor) + tf.diag(jitter + data_var)[None,:,:]
+    cov_factor = tf.einsum('sf,cs->csf', factor_loadings, tf.sqrt(all_mixture_vars))
+    covmat = tf.einsum('csf,csg->cfg', cov_factor, cov_factor) + (jitter + data_var)*tf.eye(n_features, dtype='float64')[None,:,:]
     
-    # conditioning of covariance matrices
-    #largest_magnitude_covariance = tf.reduce_max(tf.abs(covmat),axis=[1,2],keepdims=True)
-    #covmat /= largest_magnitude_covariance
-    #covmat = tf.cast(covmat,tf.float64)
-    #covmat /= tf.Print(largest_magnitude_covariance,[largest_magnitude_covariance],summarize=100,message='\n Normalizing covmats by')
-
     data = ed.MixtureSameFamily(
         mixture_distribution=tfd.Categorical(probs=all_mixture_weights),
         components_distribution=tfd.MultivariateNormalTriL(
             loc=tf.zeros((n_combinations, n_features),dtype=tf.float64),
             scale_tril=tf.linalg.cholesky(covmat),
-            #scale_tril=tf.cast(tf.sqrt(largest_magnitude_covariance),tf.float64)*tf.linalg.cholesky(tf.Print(covmat,[covmat],summarize=100)),
-            #scale_tril=tf.sqrt(largest_magnitude_covariance)*tf.linalg.cholesky(tf.Print(covmat,[covmat],summarize=100)),
-            #scale_tril=tf.sqrt(largest_magnitude_covariance)*tf.linalg.cholesky(tf.Print(covmat, [tf.check_numerics(covmat,message='covmat is nan or inf'),'Determinants:',tf.linalg.det(covmat),'max_singvals,min_singvals,cond_numbers::',condition_number(covmat)],summarize=100,message='\n')),
             name='data_component'),
             sample_shape=(n_observations,), name='data')   
     return data
-
-
-
-
-#    data = ed.MixtureSameFamily(
-#        mixture_distribution=tfd.Categorical(probs=all_mixture_weights),
-#        components_distribution=tfd.MultivariateNormalDiagPlusLowRank(loc=tf.zeros((n_combinations, n_features)), 
-#        scale_perturb_diag=None,#jitter + all_mixture_vars, 
-#        # set to None in order to use cholesky to find determinants when calculating gradients
-#        # for justification, see line 253 in https://github.com/tensorflow/tensorflow/blob/r1.10/tensorflow/contrib/distributions/python/ops/mvn_diag_plus_low_rank.py
-#        # and line 201 in https://github.com/tensorflow/tensorflow/blob/r1.10/tensorflow/python/ops/linalg/linear_operator_low_rank_update.py
-#        scale_perturb_factor=tf.einsum('sf,cs->cfs', factor_loadings, tf.sqrt(all_mixture_vars)), #tf.tile(tf.transpose(factor_loadings)[None,:,:], [n_combinations, 1, 1]) , 
-#        scale_diag=jitter + tf.tile(data_var, [n_combinations, 1]), name='data_component'), sample_shape=(n_observations,), name='data')  
-#    return data
 
 def mixtureOfGaussians(n_observations = 1000, n_components = 2, n_features = 2, mixture_component_means_mean = 0., mixture_component_means_var = 1., mixture_component_covariances_cholesky_df = None, mixture_component_covariances_cholesky_scale_tril=None,mixture_weights_concentration=None):
     if mixture_weights_concentration is None:
@@ -172,7 +132,7 @@ def lowRankMixtureOfGaussians(n_observations = 1000, n_components = 2, n_sources
     if mixture_component_covariances_cholesky_df is None:
         mixture_component_covariances_cholesky_df = n_sources + 2.
     if mixture_component_covariances_cholesky_scale_tril is None:
-        mixture_component_covariances_cholesky_scale_tril = (1./(mixture_component_covariances_cholesky_df - n_sources - 1.))*tf.eye(n_sources)
+        mixture_component_covariances_cholesky_scale_tril = (1./(mixture_component_covariances_cholesky_df - n_sources - 1.))*tf.eye(n_sources, dtype='float64')
     factor_loadings = ed.Normal(loc=0., scale=1., sample_shape=(n_sources, n_features), name='factor_loadings')
     mixture_weights = ed.Dirichlet(concentration=mixture_weights_concentration, name='mixture_weights')
     mixture_component_means = ed.Normal(loc=mixture_component_means_mean, scale=tf.sqrt(mixture_component_means_var), sample_shape=(n_components, n_sources), name='mixture_component_means')
@@ -194,7 +154,7 @@ def mixtureOfFactorAnalyzers(n_observations = 1000, n_components = 2, n_sources 
     if mixture_component_covariances_cholesky_df is None:
         mixture_component_covariances_cholesky_df = n_sources + 2.
     if mixture_component_covariances_cholesky_scale_tril is None:
-        mixture_component_covariances_cholesky_scale_tril = (1./(mixture_component_covariances_cholesky_df - n_sources - 1.))*tf.eye(n_sources)
+        mixture_component_covariances_cholesky_scale_tril = (1./(mixture_component_covariances_cholesky_df - n_sources - 1.))*tf.eye(n_sources, dtype='float64')
     factor_loadings = ed.Normal(loc=0., scale=1., sample_shape=(n_components, n_sources, n_features), name='factor_loadings')
     mixture_weights = ed.Dirichlet(concentration=mixture_weights_concentration, name='mixture_weights')
     data_var = ed.Gamma(concentration=data_var_concentration, rate=data_var_rate, sample_shape=(1,n_features), name='data_var')
